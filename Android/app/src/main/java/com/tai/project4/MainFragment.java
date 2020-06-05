a66a8b8c5446b319f7c4d19dae60182c3bc36157
taihoang <hoangthanhtai1508@gmail.com>
2020-06-05 17:17:54 +0700

    edit layout profile

--- Android/app/src/main/java/com/tai/project4/MainFragment.java
+++ Android/app/src/main/java/com/tai/project4/MainFragment.java
@@ -3,7 +3,6 @@ package com.tai.project4;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,24 +11,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.fragment.app.Fragment;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 
 public class MainFragment extends Fragment {
 
-    TextView tvName, tvEmail, tvMob, tvAddress;
+    TextView tvName, tvEmail, tvAddress;
     Button btnChangePSW, btnChangeDetails;
     Context context;
     public static final String PREFS = "PREFS";
@@ -49,7 +35,6 @@ public class MainFragment extends Fragment {
         ImageView back = view.findViewById(R.id.back);
         tvName = view.findViewById(R.id.tvName);
         tvEmail = view.findViewById(R.id.tvEmail);
-        tvMob = view.findViewById(R.id.tvMob);
         tvAddress = view.findViewById(R.id.tvAddress);
         btnChangePSW = view.findViewById(R.id.btnChangePSW);
         btnChangeDetails = view.findViewById(R.id.btnChangeDetails);
@@ -87,66 +72,12 @@ public class MainFragment extends Fragment {
             photoHeader.invalidate();
         }
 
-        ProductDetail productDetail = new ProductDetail();
-        productDetail.execute(sp.getString("loginid",null));
+        if (sp.getString("loginid", null) != null) {
+            tvName.setText(sp.getString("Name", "Name"));
+            tvEmail.setText(sp.getString("Email", "Email"));
+            tvAddress.setText(sp.getString("Address", "Address"));
+        }
 
         return view;
-    }
-
-    class ProductDetail extends AsyncTask<String, Void, String> {
-        @Override
-        protected String doInBackground(String... params) {
-            String clientDetailUrl = getResources().getString(R.string.base_url) + "getClientDetails/";
-
-            try {
-                URL url = new URL(clientDetailUrl);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoInput(true);
-                httpURLConnection.setDoOutput(true);
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                String post_Data = URLEncoder.encode("login_id", "UTF-8") + "=" + URLEncoder.encode(params[0], "UTF-8");
-
-                bufferedWriter.write(post_Data);
-                bufferedWriter.flush();
-                bufferedWriter.close();
-                outputStream.close();
-                InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-                String result = "", line = "";
-                while ((line = bufferedReader.readLine()) != null) {
-                    result += line;
-                }
-                return result;
-            } catch (Exception e) {
-                return e.toString();
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-            try {
-
-                JSONArray clientDetailArray = new JSONArray(s);
-                JSONObject json_data = new JSONObject();
-                json_data = clientDetailArray.getJSONObject(0);
-
-                    tvName.setText(json_data.getString("name"));
-                    tvEmail.setText(json_data.getString("email"));
-                    tvMob.setText(json_data.getString("mobile"));
-                    tvAddress.setText(json_data.getString("address"));
-
-            } catch (JSONException e) {
-                Toast.makeText(context, "No Internet Connection", Toast.LENGTH_SHORT).show();
-            }
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
     }
 }
